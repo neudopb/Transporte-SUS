95bc7308975858a7388b4eb171c9a6651b9221dd
mgirlene <mgirlene.silva0908@gmail.com>
2021-09-27 13:27:11 -0300

    Implementando a localização com origem e destino

--- src/pages/accounts/Localization.js
+++ src/pages/accounts/Localization.js
@@ -4,33 +4,42 @@ import MapView from 'react-native-maps';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import MapViewDirections from 'react-native-maps-directions';
-// import config from "../../../config"
+import config from "../../../config"
 
 export function Localization(){
 
+    const mapEl = useRef(null);
     const [origin, setOrigin] = useState(null);
     const [destination, setDestination] = useState(null);
 
     async function local(){
         const { status} = await Location.requestForegroundPermissionsAsync();
-        console.log(status);
+
         if (status === 'granted') {
-            let location = await Location.getLastKnownPositionAsync({accuracy: 6,});
+            let location = await Location.getLastKnownPositionAsync({accuracy: Location.Accuracy.Balanced,});
+            console.log(location);
+            console.log("aquiiiiiii");
+            console.log(location.coords.latitude);
+            console.log(location.coords.longitude);
+
             setOrigin({
                 latitude: location.coords.latitude,
                 longitude: location.coords.longitude,
                 latitudeDelta: 0.00922,
                 longitudeDelta: 0.00421,
 
-            })
+            });
 
             setDestination({
-                latitude: -6.2791652,
-                longitude: -38.6115984,
+                latitude: -6.400637,
+                longitude: -38.857329,
                 latitudeDelta: 0.00922,
                 longitudeDelta: 0.00421,
 
-            })
+            });
+
+            console.log("Destination");
+            console.log(destination);
           } else {
             throw new Error('É necessário autorização');
           }
@@ -44,33 +53,38 @@ export function Localization(){
 
     return(
         <View style={styles.container}>
-            {/* <MapView 
-                style={styles.map}
-                initialRegion ={origin}
-                showUserLocation= {true}
-                loadingEnabled = {true}
-            >    
-            </MapView> */}
-
             <MapView 
                 style={styles.map}
                 initialRegion ={origin}
                 showUserLocation= {true}
                 loadingEnabled = {true}
+                ref = {mapEl}
             > 
-                {/* {destination && 
+                {destination && 
                 
                     <MapViewDirections
+                        lineDashPattern={[0]}
                         origin = {origin}
                         destination = {destination}
                         apikey = {config.google_api}
                         strokeWidth = {3}
                         onReady = {result => {
                             console.log(result);
-                        }}
+                                mapEl.current.fitToCoordinates(
+                                    result.coordinates,{
+                                        edgePadding:{
+                                            top:50,
+                                            bottom: 50,
+                                            left:50,
+                                            right:50
+                                        }
+                                    }
+                                )
+                            }
+                        }
                     
                     />
-                } */}
+                }
                 
             </MapView>
 
